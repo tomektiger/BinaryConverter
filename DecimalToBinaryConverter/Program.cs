10b6cf81b7507973f37f73b3a53813bf2e4b08b0
Tomasz Kremiec <tkremiec@gmail.com>
2022-02-08 21:36:34 +0100

    1.0.3

--- DecimalToBinaryConverter/Program.cs
+++ DecimalToBinaryConverter/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DecimalToBinaryConverter;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DecimalToBinaryConverter;
 
 public class Program
 {
@@ -17,6 +19,7 @@ public class Program
         Console.WriteLine("1. Użyć kalkulatora");
         Console.WriteLine("2. Przekonwertować liczbę na ZM,U1,U2");
     }
+    [SuppressMessage("ReSharper.DPA", "DPA0001: Memory allocation issues")]
     public static void MakeChoice()
     {
         Console.Write("Twój wybór: ");
@@ -31,15 +34,35 @@ public class Program
                     var firstNumber = double.Parse(Console.ReadLine());
                     Console.Write("Wpisz drugą liczbę: ");
                     var secondNumber = double.Parse(Console.ReadLine());
-
-                    var firstNumberInBinary = decimalToBinary(firstNumber, 10000).TrimEnd('0');
-                    var secondNumberInBinary = decimalToBinary(secondNumber, 10000).TrimEnd('0');
-
                     
-                    Console.WriteLine($"{firstNumber} in binary is: {firstNumberInBinary}");
-                    Console.WriteLine($"{secondNumber} in binary is: {secondNumberInBinary}");
+                    var firstNumberAbs = Math.Abs(firstNumber);
+                    var secondNumberAbs = Math.Abs(secondNumber);
+                    var firstNumberInBinary = decimalToBinary(firstNumber, 10000).TrimEnd('0');
+                    var firstNumberAbsInBinary = decimalToBinary(firstNumberAbs, 10000).TrimEnd('0');
+                    var secondNumberAbsInBinary = decimalToBinary(secondNumberAbs, 10000).TrimEnd('0');
+                    var secondNumberInBinary = decimalToBinary(secondNumber, 10000).TrimEnd('0');
+                    
+                        if (firstNumber < 0)
+                        {
+                            Console.WriteLine($"{firstNumber} in binary is: -{firstNumberAbsInBinary}");
 
-                    MakeOperations(firstNumber, secondNumber);
+                        }
+
+                        if (secondNumber < 0)
+                        {
+                            Console.WriteLine($"{secondNumber} in binary is: -{secondNumberAbsInBinary}");
+
+                        }
+                        if(firstNumber>0){
+                            Console.WriteLine($"{firstNumber} in binary is: {firstNumberInBinary}");
+                        }
+
+                        if (secondNumber > 0)
+                        {
+                            Console.WriteLine($"{secondNumber} in binary is: {secondNumberInBinary}");
+                        }
+
+                        MakeOperations(firstNumber, secondNumber);
                     
                     Console.Write("\nDo you want to repeat ? [y,Y/n,N]: ");
                     repeat = char.Parse(Console.ReadLine());
@@ -78,34 +101,41 @@ public class Program
         PrintStars();
         Console.WriteLine();
     }
-    public static void MakeOperations(double num1,double num2)
+    public static void MakeOperations(double num1, double num2)
     {
         Console.Write("Please choose the operator [+,-,*,/]: ");
         var userOperator = Console.ReadLine();
 
-        if (userOperator == "+")
+        if (num1 < 0 || num2 < 0)
         {
-            Console.WriteLine($"{num1} + {num2} in binary is: {PerformAddition(num1, num2)}");
+            var num1Abs = Math.Abs(num1);
+            var num2Abs = Math.Abs(num2);
+
+            if (userOperator == "+")
+            {
+                Console.WriteLine($"{num1} + {num2} in binary is: {PerformAddition(num1, num2)}");
+            }
+            else if (userOperator == "-")
+            {
+                Console.WriteLine($"{num1} - {num2} in binary is: {PerformSubtraction(num1, num2)}");
+            }
+            else if (userOperator == "*")
+            {
+                Console.WriteLine($"{num1} * {num2} in binary is : {PerformMultiplication(num1, num2)}");
+            }
+            else if (userOperator == "/")
+            {
+                Console.WriteLine($"{num1} / {num2} in binary is: {PerformDivision(num1, num2)}");
+            }
         }
-        else if (userOperator == "-")
-        {
-            Console.WriteLine($"{num1} - {num2} in binary is: {PerformSubtraction(num1, num2)}");
-        }
-        else if (userOperator == "*")
-        {
-            Console.WriteLine($"{num1} * {num2} in binary is : {PerformMultiplication(num1, num2)}");
-        }
-        else if (userOperator == "/")
-        {
-            Console.WriteLine($"{num1} / {num2} in binary is: {PerformDivision(num1, num2)}");
-        }
-}
+    }
     public static string Array2String<T>(IEnumerable<T> list)
     {
         return string.Join("", list);
     }
     public static void ConvertDecimalToZmU1U2(int liczba)
     {
+        var number = liczba;
         int negacja = 0;
         byte[] U2 = new byte[32];
         byte[] ZM = new byte[32];
@@ -133,6 +163,16 @@ public class Program
         }
         U1[0] = ZM[0];
         //dla kontroli
+        if (number < 0)
+        {
+            Console.WriteLine("Binary: -" + decimalToBinary(Math.Abs(number), 10000).TrimEnd('0'));
+        }
+
+        if (number > 0)
+        {
+            Console.WriteLine("Binary: " + decimalToBinary(number, 10000).TrimEnd('0'));
+
+        }
         Console.WriteLine("U2: "+Array2String(U2).TrimStart('0'));
         Console.WriteLine("ZM: "+Array2String(ZM).TrimStart('0'));
         Console.WriteLine("U1: "+Array2String(U1).TrimStart('0'));
@@ -208,33 +248,83 @@ public class Program
     {
 
         double sum = num1 + num2;
-        
-        var sumInBinary = decimalToBinary(sum, 10000).TrimEnd('0');
+        Console.WriteLine($"{num1} + {num2} = {sum}");
+        string sumInBinary = " ";
+
+        if (sum > 0)
+        {
+            sumInBinary = decimalToBinary(sum, 10000).TrimEnd('0');
+            return sumInBinary;
+        }
+
+        if (sum < 0)
+        {
+            sum = Math.Abs(sum); 
+            sumInBinary = "-"+decimalToBinary(sum, 10000).TrimEnd('0');
+        }
 
         return sumInBinary;
-
     }
     public static string PerformMultiplication(double num1, double num2)
     {
         double multiplication = num1 * num2;
-        
-        var multiplicationInBinary = decimalToBinary(multiplication, 10000).TrimEnd('0');
+        Console.WriteLine($"{num1} * {num2} = {multiplication}");
+        string multiplicationInBinary = " ";
 
+        if (multiplication > 0)
+        {
+            multiplicationInBinary = decimalToBinary(multiplication, 10000).TrimEnd('0');
+        }
+        if (multiplication < 0)
+        {
+            multiplication = Math.Abs(multiplication);
+            multiplicationInBinary = "-"+decimalToBinary(multiplication, 10000).TrimEnd('0');
+        }
         return multiplicationInBinary;
     }
     public static string PerformDivision(double num1, double num2)
     {
-        double division = num1 / num2;
         
-        var divisionInBinary = decimalToBinary(division, 10000).TrimEnd('0');
+        double division = num1 / num2;
+        Console.WriteLine($"{num1} / {num2} = {division}");
+        
+        string divisionInBinary = "";
+
+
+        if (division > 0)
+        {
+            divisionInBinary = decimalToBinary(division, 10000).TrimEnd('0');
+            
+        }
+
+        if (division < 0)
+        {
+            division = Math.Abs(division);
+            divisionInBinary = "-"+decimalToBinary(division, 10000).TrimEnd('0');
+            
+        }
 
         return divisionInBinary;
     }
     public static string PerformSubtraction(double num1, double num2)
     {
         double subtraction = num1-num2;
-        
-        var subtractionInBinary = decimalToBinary(subtraction, 10000).TrimEnd('0');
+        Console.WriteLine($"{num1} - {num2} = {subtraction}");
+        string subtractionInBinary = " ";
+
+        if (subtraction > 0)
+        {
+
+            subtractionInBinary = decimalToBinary(subtraction, 10000).TrimEnd('0');
+            
+        }
+
+        if (subtraction < 0)
+        {
+            subtraction = Math.Abs(subtraction);
+            subtractionInBinary = "-"+decimalToBinary(subtraction, 10000).TrimEnd('0');
+
+        }
 
         return subtractionInBinary;
     }
